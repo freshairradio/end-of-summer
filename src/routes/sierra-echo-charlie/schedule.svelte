b7ee67c584a5abb881dc491b959b702cd479ed8a
Samuel Macleod <samuel@macleod.space>
2020-08-04 18:06:55 +0100

    Add schedule

--- src/routes/sierra-echo-charlie/schedule.svelte
+++ src/routes/sierra-echo-charlie/schedule.svelte
@@ -16,44 +16,71 @@
 
     <h1 class="text-center">Schedule</h1>
 
-<h2>Schedule</h2>
+    <h2>Schedule</h2>
 
-<p>This is very tight, in that it goes right up to 9, but there are a couple areas that can be trimmed if necessary during the call.</p>
+    <p>
+      This is very tight, in that it goes right up to 9, but there are a couple
+      areas that can be trimmed if necessary during the call.
+    </p>
 
-<h2><strong>5 minutes (7.00-7.05)</em></h2>
+    <h2>
+      <strong>5 minutes (7.00-7.05)</strong>
+    </h2>
 
-<p>Waiting for delegates to join zoom, as this could be a bit of a trickle</p>
+    <p>
+      Waiting for delegates to join zoom, as this could be a bit of a trickle
+    </p>
 
-<h2><strong>15 minutes (7.05-7.20)</em> </h2>
+    <h2>
+      <strong>15 minutes (7.05-7.20)</strong>
+    </h2>
 
-<p>Opening poems from suspects. During this time breakout rooms will be assigned based on the delegates that have shown up</p>
+    <p>
+      Opening poems from suspects. During this time breakout rooms will be
+      assigned based on the delegates that have shown up
+    </p>
 
-<h2><strong>2 minutes (7.20-7.22)</em></h2>
+    <h2>
+      <strong>2 minutes (7.20-7.22)</strong>
+    </h2>
 
-<p>Breakout rooms open, with a brief minute to get settled</p>
+    <p>Breakout rooms open, with a brief minute to get settled</p>
 
-<h2><strong>33 minutes (7.22-7.55)</em></h2>
+    <h2>
+      <strong>33 minutes (7.22-7.55)</strong>
+    </h2>
 
-<p>Interrogation time, <strong>5 minutes</em> per suspect plus <strong>3 minutes</em> of allowance for switchover delays</p>
+    <p>
+      Interrogation time,
+      <strong>5 minutes</strong>
+      per suspect plus
+      <strong>3 minutes</strong>
+      of allowance for switchover delays
+    </p>
 
-<h2><strong>10 minutes (7.55-8.05)</em></h2>
+    <h2>
+      <strong>10 minutes (7.55-8.05)</strong>
+    </h2>
 
-<p>Break</p>
+    <p>Break</p>
 
-<h2><strong>25 minutes (8.05-8.30)</em></h2>
+    <h2>
+      <strong>25 minutes (8.05-8.30)</strong>
+    </h2>
 
-<p>Accusation writing</p>
+    <p>Accusation writing</p>
 
-<h2><strong>15 minutes (8.30-8.45)</em></h2>
+    <h2>
+      <strong>15 minutes (8.30-8.45)</strong>
+    </h2>
 
-<p>Accusation speeches</p>
+    <p>Accusation speeches</p>
 
-<h2><strong>15 minutes (8.45-9.00)</em></h2>
+    <h2>
+      <strong>15 minutes (8.45-9.00)</strong>
+    </h2>
 
-<p>Confessions &amp; non-confessions</p>
-
-
-
+    <p>Confessions &amp; non-confessions</p>
 
   </article>
 </main>
